740790c339f7c0c8a4d8bc91f68c8265a701687e
VatsSerhiy <91936606+VatsSerhiy@users.noreply.github.com>
2023-06-20 23:30:45 +0300

    Fix bug create badge

--- routes/badge.route.js
+++ routes/badge.route.js
@@ -9,10 +9,9 @@ const {
 router.get("/findBadge", findBadge);
 
 router.get("/create", async (req, res, next) => {
-    // if (req.session.isAuthenticated) {
-    //     res.render('index.ejs', {isAuthenticated: req.session.isAuthenticated});
-    // } else res.redirect('/auth/signin');
-    res.render('index.ejs', {isAuthenticated: req.session.isAuthenticated});
+    if (req.session.isAuthenticated) {
+        res.render('index.ejs', {isAuthenticated: req.session.isAuthenticated});
+    } else res.redirect('/auth/signin');
 });
 
 router.post("/save", saveBadge);
